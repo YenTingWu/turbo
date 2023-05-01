c103e6dc495b7392127899bf7ef8a77e508d7b6b
Greg Soltis <greg@goldfiglabs.com>
2023-05-01 10:21:05 -0700

    WIP on rust impl of file hashing

--- crates/turborepo-scm/src/package_deps.rs
+++ crates/turborepo-scm/src/package_deps.rs
@@ -1,13 +1,12 @@
 use std::{
     collections::HashMap,
+    ffi::OsString,
     io::{BufRead, BufReader},
     process::{Command, Stdio},
 };
 
 use anyhow::{anyhow, Result};
-use turbopath::{
-    AbsoluteSystemPathBuf, AnchoredSystemPathBuf, RelativeSystemPathBuf, RelativeUnixPathBuf,
-};
+use turbopath::{AbsoluteSystemPathBuf, AnchoredSystemPathBuf, RelativeUnixPathBuf};
 
 type GitHashes = HashMap<RelativeUnixPathBuf, String>;
 
@@ -18,14 +17,69 @@ pub fn get_package_deps(
 ) -> Result<GitHashes> {
     let result = if inputs.len() == 0 {
         let full_pkg_path = turbo_root.resolve(package_path);
-        git_ls_tree(&full_pkg_path)?
+        let mut hashes = git_ls_tree(&full_pkg_path)?;
+        let to_hash = append_git_status(turbo_root, inputs, &mut hashes)?;
+        hashes
     } else {
         unimplemented!()
     };
     Ok(result)
 }
 
+fn append_git_status(
+    root_path: &AbsoluteSystemPathBuf,
+    patterns: &[&str],
+    hashes: &mut GitHashes,
+) -> Result<Vec<RelativeUnixPathBuf>> {
+    let mut to_hash = Vec::new();
+    let mut args = vec!["status", "--untracked-files", "--no-renames", "-z", "--"];
+    if patterns.len() == 0 {
+        args.push(".");
+    } else {
+        let mut patterns = Vec::from(patterns);
+        args.append(&mut patterns);
+    }
+    let mut git = Command::new("git")
+        .args(args.as_slice())
+        .current_dir(root_path)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+    {
+        let stdout = git
+            .stdout
+            .as_mut()
+            .ok_or_else(|| anyhow!("failed to get stdout for git status"))?;
+        let mut reader = BufReader::new(stdout);
+        let mut buffer = Vec::new();
+        loop {
+            buffer.clear();
+            {
+                let bytes_read = reader.read_until(b'\0', &mut buffer)?;
+                if bytes_read == 0 {
+                    break;
+                }
+                {
+                    let (filename, x, y) = parse_status(&buffer)?;
+                    let filename = String::from_utf8(filename.to_vec())?;
+                    let filename = std::path::PathBuf::from(OsString::from(filename));
+                    let path = RelativeUnixPathBuf::new(filename)?;
+                    let is_delete = x == b'D' || y == b'D';
+                    if is_delete {
+                        hashes.remove(&path);
+                    } else {
+                        to_hash.push(path);
+                    }
+                }
+            }
+        }
+    }
+    git.wait()?;
+    Ok(to_hash)
+}
+
 fn git_ls_tree(root_path: &AbsoluteSystemPathBuf) -> Result<GitHashes> {
+    let mut hashes = GitHashes::new();
     let mut git = Command::new("git")
         .args(&["ls-tree", "-r", "-z", "HEAD"])
         .current_dir(root_path)
@@ -37,17 +91,91 @@ fn git_ls_tree(root_path: &AbsoluteSystemPathBuf) -> Result<GitHashes> {
             .stdout
             .as_mut()
             .ok_or_else(|| anyhow!("failed to get stdout for git ls-tree"))?;
-        let reader = BufReader::new(stdout);
+        let mut reader = BufReader::new(stdout);
         let mut buffer = Vec::new();
         loop {
-            let bytes_read = reader.read_until('\0', &mut buffer)?;
-            if bytes_read == 0 {
-                break;
+            buffer.clear();
+            {
+                let bytes_read = reader.read_until(b'\0', &mut buffer)?;
+                if bytes_read == 0 {
+                    break;
+                }
+                {
+                    let (filename, hash) = parse_ls_tree(&buffer)?;
+                    let filename = String::from_utf8(filename.to_vec())?;
+                    let filename = std::path::PathBuf::from(OsString::from(filename));
+                    let hash = String::from_utf8(hash.to_vec())?;
+                    let path = RelativeUnixPathBuf::new(filename)?;
+                    hashes.insert(path, hash);
+                }
             }
         }
     }
     git.wait()?;
-    unimplemented!()
+    Ok(hashes)
+}
+
+fn git_hash_object(files_to_hash: Vec<RelativeUnixPathBuf>, &mut hashes: GitHashes) -> Result<()> {
+    let mut git = Command::new("git")
+        .args(&["hash-object", "--stdin-paths"])
+        .current_dir(root_path)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+    {
+        let stdin = git
+            .stdin
+            .as_mut()
+            .ok_or_else(|| anyhow!("failed to get stdin for git hash-object"));
+        let stdout = git
+            .stdout
+            .as_mut()
+            .ok_or_else(|| anyhow!("failed to get stdout for git ls-tree"))?;
+    }
+}
+
+fn parse_status(i: &[u8]) -> Result<(&[u8], u8, u8)> {
+    use nom::Finish;
+    match nom_parse_status(i).finish() {
+        Ok((_, tup)) => Ok(tup),
+        Err(e) => Err(anyhow!("nom: {:?} {}", e, std::str::from_utf8(e.input)?)),
+    }
+}
+
+fn nom_parse_status(i: &[u8]) -> nom::IResult<&[u8], (&[u8], u8, u8)> {
+    let (i, x) = nom::bytes::complete::take(1usize)(i)?;
+    let (i, y) = nom::bytes::complete::take(1usize)(i)?;
+    let (i, _) = nom::character::complete::space1(i)?;
+    let (i, filename) = non_space(i)?;
+    Ok((i, (filename, x[0], y[0])))
+}
+
+fn parse_ls_tree(i: &[u8]) -> Result<(&[u8], &[u8])> {
+    use nom::Finish;
+    match nom_parse_ls_tree(i).finish() {
+        Ok((_, tup)) => Ok(tup),
+        Err(e) => Err(anyhow!("nom: {:?}", e)),
+    }
+}
+
+fn nom_parse_ls_tree(i: &[u8]) -> nom::IResult<&[u8], (&[u8], &[u8])> {
+    let (i, _) = non_space(i)?;
+    let (i, _) = nom::character::complete::space1(i)?;
+    let (i, _) = non_space(i)?;
+    let (i, _) = nom::character::complete::space1(i)?;
+    let (i, hash) = hash(i)?;
+    let (i, _) = nom::character::complete::space1(i)?;
+    let (i, filename) = non_space(i)?;
+    Ok((i, (filename, hash)))
+}
+
+fn non_space(i: &[u8]) -> nom::IResult<&[u8], &[u8]> {
+    nom::bytes::complete::is_not(" \0")(i)
+}
+
+fn hash(i: &[u8]) -> nom::IResult<&[u8], &[u8]> {
+    nom::bytes::complete::take(40usize)(i)
 }
 
 #[cfg(test)]
